37dcd41be918c6773651d4cfb44eed5d1bc5447b
Grzegorz Ciołek <contact@ciolek.co.uk>
2026-06-17 17:02:19 +0100

    feat(nvim): add buffer manipulation binds
    Add binds for switching between buffers (both directly and with
    telescope) and for closing the active buffer.

--- .config/nvim/lua/core/remap.lua
+++ .config/nvim/lua/core/remap.lua
@@ -1,2 +1,6 @@
 vim.keymap.set('v', 'J', ':m \'>+1<CR>gv=gv;\'')
 vim.keymap.set('v', 'K', ':m \'<-2<CR>gv=gv;\'')
+
+vim.keymap.set('n', '<leader>bc', ':bd<CR>')
+vim.keymap.set('n', '<leader>bn', ':bnext<CR>')
+vim.keymap.set('n', '<leader>bp', ':bprevious<CR>')
