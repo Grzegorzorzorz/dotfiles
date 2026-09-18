b300d9a842ac69ba9c14a81551a37873d1e18063
Grzegorz Ciołek <contact@ciolek.co.uk>
2026-09-18 16:06:54 +0100

    feat(hypr): reposition monitor layout

--- .config/hypr/hyprland/monitors.lua
+++ .config/hypr/hyprland/monitors.lua
@@ -1,14 +1,14 @@
 hl.monitor({
    output = 'eDP-1',
    mode = '2560x1440@240',
-   position = '0x0',
+   position = '1680x0',
    scale = 1.25,
 })
 
 hl.monitor({
-   output = 'DP-3',
+   output = 'desc:Dell Inc. DELL P2213 Y57VF33D156M',
    mode = '1680x1050@59.95',
-   position = '2048x0',
+   position = '0x0',
    scale = 1,
    transform = 0,
 })
